cff65827bfe254f939c42eac53fe33031350bb92
Bobertkiller <felipe.mazzeo.barbosa@outlook.com>
2025-03-06 20:32:35 -0300

    commit inicial

---
+++ src/Header/Header.tsx
@@ -0,0 +1,19 @@
+function Header(){
+    return(
+        <header>
+            <h1>Meu Site</h1>
+            <nav>
+                <ul>
+                    <li><a href = "#">Home</a></li>
+                    <li><a href = "#">Sobre</a></li>
+                    <li><a href = "#">Serviços</a></li>
+                    <li><a href = "#">contato</a></li>
+                    <li><a href = "#">Cu do Ovo</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+export default Header
